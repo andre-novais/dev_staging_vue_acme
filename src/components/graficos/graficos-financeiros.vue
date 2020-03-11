f804a6403e5e6f3403b42acf81938d1d1a43faba
Andre Novais <andre.novais@daemon.local>
2020-03-11 16:02:09 -0300

    e

--- src/components/graficos/graficos-financeiros.vue
+++ src/components/graficos/graficos-financeiros.vue
@@ -5,7 +5,6 @@
     </div>
 </template>
 
-
 <script>
 var moment =require('moment')
 import {epoch_to_month} from '../funcoes/epoch_to_month'
@@ -19,48 +18,53 @@ export default {
         }
     },
     created(){
-        this.$http.get("https://projeto-acme.herokuapp.com/financeiro", {headers: {'Access-Control-Allow-Origin': "*"}})
-            .then(res =>this.list = epoch_to_month(res["body"]))
+        
     },
     mounted () {
-        var receitas_custos = new Grafico('receitas e custos variaveis');
-        setTimeout(()=>{
-            receitas_custos.setSeries(
-                this.list.filter(json=>json['ds_tipificacao']=='receita'),
-                'occured_at',
-                'vl_valor',
-                'receitas',
-                'line'
-            );
-            receitas_custos.setSeries(
-                this.list.filter(json=>json['ds_tipificacao']=='custo'),
-                'occured_at',
-                'vl_valor',
-                'custos',
-                'line'
-            );
-            receitas_custos.chart.yAxis(0).labels().format('R${%value}')
-            
-            
         
-            receitas_custos.desenha('receitas_custos');
-            var entrada_saida = new Grafico('Funding e Necessidade de Caixa');
-            entrada_saida.setSeries(
-                this.list.filter(json=>json['ds_tipificacao']=='entrada_com_contrapartida'),
-                'occured_at',   
-                'vl_valor',
-                'Funding',
-                'line'
-            );
-            entrada_saida.setSeries(
-                this.list.filter(json=>json['ds_tipificacao']=='despesa'),
-                'occured_at',
-                'vl_valor',
-                'Despendio Administrativo',
-                'line'
-            );
-            entrada_saida.desenha('receitas_custos');
-        },1000)
+        this.$http.get("https://projeto-acme.herokuapp.com/financeiro", {headers: {'Access-Control-Allow-Origin': "*"}})
+            .then(res =>{
+                this.list = epoch_to_month(res["body"])
+                
+                var receitas_custos = new Grafico('receitas e custos variaveis');
+
+                receitas_custos.setSeries(
+                    this.list.filter(json=>json['ds_tipificacao']=='receita'),
+                    'occured_at',
+                    'vl_valor',
+                    'receitas',
+                    'line'
+                );
+                receitas_custos.setSeries(
+                    this.list.filter(json=>json['ds_tipificacao']=='custo'),
+                    'occured_at',
+                    'vl_valor',
+                    'custos',
+                    'line'
+                );
+                receitas_custos.desenha('receitas_custos')
+                receitas_custos.chart.yAxis(0).labels().format('R${%value}')
+                
+                var entrada_saida = new Grafico('Funding e Necessidade de Caixa');
+                entrada_saida.setSeries(
+                    this.list.filter(json=>json['ds_tipificacao']=='entrada_com_contrapartida'),
+                    'occured_at',   
+                    'vl_valor',
+                    'Funding',
+                    'line'
+                );
+                entrada_saida.setSeries(
+                    this.list.filter(json=>json['ds_tipificacao']=='despesa'),
+                    'occured_at',
+                    'vl_valor',
+                    'Despendio Administrativo',
+                    'line'
+                );
+                entrada_saida.desenha('entrada_saida');
+                entrada_saida.chart.yAxis(0).labels().format('R${%value}')
+                
+            })
+
     },
     methods : {
         desenha:desenha
